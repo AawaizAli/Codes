a2d8d9d24b932c42539a34a8083a324d5ec9af7e
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2023-05-04 12:17:28 +0500

    Add files via upload

---
+++ Python/binary sort/main.py
@@ -0,0 +1,30 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr)-1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        # Check if target is present at mid
+        if arr[mid] == target:
+            return mid
+        
+        # If target is greater, ignore left half
+        elif arr[mid] < target:
+            left = mid + 1
+        
+        # If target is smaller, ignore right half
+        else:
+            right = mid - 1
+            
+    # Target is not present in the array
+    return -1
+
+arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+target = 14
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
